3024124174dfae1de1dca80688155153225a9839
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-30 16:07:35 +0300

    1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,26 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const ulGalleryEl = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join('');
+
+ulGalleryEl.insertAdjacentHTML('afterbegin', markup);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
