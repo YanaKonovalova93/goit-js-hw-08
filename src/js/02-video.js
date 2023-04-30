1c91e44174307a3466f173af9066c2ea5a320c34
YanaKonovalova93 <mikobacteria@gmail.com>
2023-04-30 17:29:20 +0300

    3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const STORAGE_KEY = 'videoplayer-current-time';
+const player = new Player(iframe);
+
+
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const videoPlayTime = Number(localStorage.getItem(STORAGE_KEY));
+
+player
+  .setCurrentTime(videoPlayTime)
+  .then(function (seconds) {
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
 
